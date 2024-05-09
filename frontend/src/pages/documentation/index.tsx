5223f7b6418e512cc4a896188cbe08f373bfbd29
Michael Desmond <mdesmond@us.ibm.com>
2024-05-09 15:05:15 -0400

    feat: first cut of documentation (#71)
    Co-authored-by: Michael Desmond <mdesmond@Michaels-MacBook-Pro.local>

--- frontend/src/pages/documentation/index.tsx
+++ frontend/src/pages/documentation/index.tsx
@@ -1,4 +1,4 @@
-import { Content } from '@carbon/react'
+import { Content, Link } from '@carbon/react'
 import classes from '@styles/SingleExampleEvaluation.module.scss'
 
 import { AppHeader } from '@components/AppHeader/AppHeader'
@@ -10,43 +10,33 @@ const Documentation = () => {
       <AppHeader displaySidenav={false} />
       <Content>
         <article className={classes['documentation']}>
-          <h1 style={{ marginBottom: '2rem' }}>Documentation</h1>
+          <h3 style={{ marginBottom: '1rem' }}>EvalAssist & LLM-as-a-judge</h3>
           <hr className={classes['horizontal-divider']} />
           <h2>Overview</h2>
           <p>
-            <strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-            into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-            software like Aldus PageMaker including versions of Lorem Ipsum
+            <strong>EvalAssist</strong> is a development environment for LLM-as-a-judge evaluation criteria. We
+            currently support rubric style criteria where the evaluation is described in terms of a question and a set
+            of predefined answers.
           </p>
-          <h2>Criteria definition</h2>
+          <h2>Context, response & criteria</h2>
           <p>
-            <strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-            into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-            software like Aldus PageMaker including versions of Lorem Ipsum
+            LLM-as-a-judge evaluations are comprised of an <strong>input context</strong>, a{' '}
+            <strong>response to evaluate </strong>
+            and a <strong>criteria definition</strong>. The input context provides contextual information to the LLM
+            judge, this could be a question in a Q/A evaluation scenario, or a reference document and a question in a
+            RAG Q/A scenario. The response to evaluate is the text that we want the LLM to evaluate, subject to the
+            input context. Finally the criteria describes the details of the evaluation itself.
           </p>
-          <h2>Installation</h2>
+          <h2>Exporting criteria & evaluating larger datasets</h2>
           <p>
-            <strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-            into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-            software like Aldus PageMaker including versions of Lorem Ipsum
-          </p>
-          <h2>Usage</h2>
-          <p>
-            <strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-            into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-            software like Aldus PageMaker including versions of Lorem Ipsum
+            You can export criteria from EvalAssist in json format by selecting the JSON tab on the evaluation criteria
+            and copying the json data. We provide a standalone LLM-as-a-judge library that you can use to apply criteria
+            developed in EvalAssist to larger datasets.
+            <br />
+            <br />
+            <Link href="https://github.ibm.com/AIExperience/llm-as-a-judge">
+              https://github.ibm.com/AIExperience/llm-as-a-judge
+            </Link>
           </p>
         </article>
       </Content>
