f692b67eae7b5bbb1476faa8f41fc4aa7fbdbaee
J. Johnson <jmjohnson@us.ibm.com>
2024-05-21 11:06:53 -0400

    feat: changes to documentation issues 101 and 102 (#118)

--- frontend/src/pages/documentation/index.tsx
+++ frontend/src/pages/documentation/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { Content, Link } from '@carbon/react'
 
 import { AppHeader } from '@components/AppHeader/AppHeader'
@@ -16,6 +18,20 @@ const Documentation = () => {
           <strong>EvalAssist</strong> is a development environment for LLM-as-a-judge evaluation criteria. We currently
           support rubric style criteria where the evaluation is described in terms of a question and a set of predefined
           answers.
+        </p>
+        <h2>Get API Key</h2>
+        <p>
+          You can obtain and copy your API key from{' '}
+          <Link href="https://bam.res.ibm.com/">https://bam.res.ibm.com/</Link> under the documentation section (see
+          highlighted portion below)
+          <br />
+          <br />
+          <Image
+            src="/images/api_key_help.png"
+            alt="Image describing location of API key on BAM page"
+            width="642"
+            height="385"
+          />
         </p>
         <h2>Context, response & criteria</h2>
         <p>
@@ -36,6 +52,20 @@ const Documentation = () => {
           <Link href="https://github.ibm.com/AIExperience/llm-as-a-judge">
             https://github.ibm.com/AIExperience/llm-as-a-judge
           </Link>
+        </p>
+        <h2>Sponsor Users</h2>
+        <p>
+          We are deeply grateful to the sponsored users listed below, whose invaluable feedback has greatly contributed
+          to refining and enhancing our tools.
+          <br /> <br />
+          Top contributors (in alphabetical order): Andrew R. Freed, Andre Lopes, Bill Murdock, Charley Beller, Connie
+          He, Davidson Siga, Huaiyu Zhu, Michael Choie, Shobhi Varshney, Sonali Rajendra, Yannis Katsis
+          <br /> <br />
+          If you&apos;re interested in becoming part of our sponsored user program and engaging in our user study to
+          test our tool and offer feedback,{' '}
+          <Link href="https://airtable.com/appBlXR5AJ5v3dHDN/shrtDukPTBiP7VjJd">please fill out this form</Link>. Your
+          input is pivotal in our continuous efforts to enhance our tool, making it more impactful and generating
+          valuable insights for publication.
         </p>
       </article>
     </Content>
