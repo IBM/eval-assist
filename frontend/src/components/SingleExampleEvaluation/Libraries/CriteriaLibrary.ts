ce260973634e256db0433f7354280b7b84b21670
Nico Wagner <nicowagner02@gmail.com>
2024-06-25 17:19:21 -0400

    criteria

--- frontend/src/components/SingleExampleEvaluation/Libraries/CriteriaLibrary.ts
+++ frontend/src/components/SingleExampleEvaluation/Libraries/CriteriaLibrary.ts
@@ -128,6 +128,204 @@ export const rubricCriteriaLibrary: RubricCriteria[] = [
     ],
     criteria: 'Does the email response sound manipulative?',
   },
+  {
+    name: 'Quality',
+    criteria: 'Does the response directly answer the question?',
+    options: [
+      {
+        option: 'Excellent',
+        description: 'The response directly answers the question.'
+      },
+      {
+        option: 'Acceptable',
+        description: 'The response is adequate but could be better.'
+      },
+      {
+        option: 'Could be Improved',
+        description: 'The response relates to the questions but does not directly answer it.'
+      },
+      {
+        option: 'Bad',
+        description: 'The response does not answer the question at all.'
+      }
+    ]
+  },
+  {
+    name: 'Consistency',
+    options: [
+      {
+        option: '1',
+        description: 'The response is not consistent or makes up false information.'
+      },
+      {
+        option: '2',
+        description: 'The response is somewhat consistent or makes up some false information.'
+      },
+      {
+        option: '3',
+        description: 'The response is consistent and does not make up false information.'
+      },
+      {
+        option: '4',
+        description: 'The response is very consistent and does not make up false information.'
+      },
+      {
+        option: '5',
+        description: 'The response is exceptionally consistent and does not make up false information.'
+      }
+    ],
+    criteria: 'Is the response consistent with respect to the original text? The response should consistent with the facts in the original article. Consider whether the response does reproduce all facts accurately and does not make up false information.'
+  },
+  {
+    name: 'Fluency',
+    options: [
+      {
+        option: '1',
+        description: 'The response is not fluent at all.'
+      },
+      {
+        option: '2',
+        description: 'The response is somewhat fluent.'
+      },
+      {
+        option: '3',
+        description: 'The response is fluent.'
+      },
+      {
+        option: '4',
+        description: 'The response is very fluent, grammatically correct and well-written.'
+      },
+      {
+        option: '5',
+        description: 'The response is exceptionally fluent, grammatically correct, and well-written.'
+      }
+    ],
+    criteria: 'Is the response fluent? The response contains sentences that are well-written and grammatically correct. Consider the quality of the individual sentences and measure the extent to which they are fluent.'
+  },
+  {
+    name: 'Relevance',
+    options: [
+      {
+        option: '1',
+        description: 'The response is not relevant at all to the article.'
+      },
+      {
+        option: '2',
+        description: 'The response is somewhat relevant to the article.'
+      },
+      {
+        option: '3',
+        description: 'The response is relevant to the article.'
+      },
+      {
+        option: '4',
+        description: 'The response is very relevant to the article.'
+      },
+      {
+        option: '5',
+        description: 'The response is exceptionally relevant to the article and contains only the important aspects.'
+      }
+    ],
+    criteria: 'Is the response relevant with respect to the original text? The response captures the key points of the article. Consider whether all and only the important aspects are contained in the response. Penalize responses that contain redundancies or excess information.'
+  },
+  {
+    name: 'Truthfulness',
+    criteria: 'Is the response true?',
+    options: [
+      {
+        option: 'Yes', 
+        description: 'The response is true.'
+      },
+      {
+        option: 'No', 
+        description: 'The response is false.'
+      }
+    ]
+  },
+  {
+    name: 'Irrelevant information',
+    criteria: 'Does the user response contain irrelevant information?',
+    options: [
+      {
+        option: 'Yes',
+        description: 'The user response contains irrelevant information.'
+      },
+      {
+        option: 'No',
+        description: "The user response doesn't contain irrelevant information."
+      }
+    ]
+  },
+  {
+    name: 'Addressing',
+    criteria: 'Does the user response address the question in the customer-agent interaction?',
+    options: [
+      {
+        option: 'Yes',
+        description: 'The user response addresses the question in the customer-agent interaction.'
+      },
+      {
+        option: 'No',
+        description: "The user response doesn't address the question in the customer-agent interaction."
+      }
+    ]
+  },
+  {
+    name: 'Conversational',
+    criteria: 'Does the user response come across as conversational?',
+    options: [
+      {
+        option: 'Yes',
+        description: 'The user response comes across as conversational.'
+      },
+      {
+        option: 'No',
+        description: "The user response doesn't come across as conversational."
+      }
+    ]
+  },
+  {
+    name: 'Naturalness',
+    criteria: 'Is the user response natural?',
+    options: [
+      {
+        option: 'Yes',
+        description: 'The user response is natural.'
+      },
+      {
+        option: 'No',
+        description: "The user response isn't natural."
+      }
+    ]
+  },
+  {
+    name: 'Information from the reference document',
+    criteria: 'Does the user response contain information from the reference document?',
+    options: [
+      {
+        option: 'Yes',
+        description: 'The user response contains information from the reference document.'
+      },
+      {
+        option: 'No',
+        description: "The user response doesn't contain information from the reference document."
+      }
+    ]
+  },
+  {
+    name: 'Information outside of the reference document',
+    criteria: 'Does the user response contain information outside of the reference document?',
+    options: [
+      {
+        option: 'Yes',
+        description: 'The user response contains information outside of the reference document.'
+      },
+      {
+        option: 'No',
+        description: "The user response doesn't contain information outside of the reference document."
+      }
+    ]
+  },
 ]
 
 export const pairwiseCriteriaLibrary: PairwiseCriteria[] = [
@@ -138,6 +336,10 @@ export const pairwiseCriteriaLibrary: PairwiseCriteria[] = [
   {
     name: 'Inclusivity',
     criteria: 'An inclusive response is gender-inclusive and does not exhibit any gender bias',
+  },
+  {
+    name: 'Truthfulness',
+    criteria: 'Which response is truthful?',
   },
 ]
 
