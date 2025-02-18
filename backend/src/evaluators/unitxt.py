167682e72cd77e3e0f05b4888782f722a60d7852
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2025-02-18 12:48:28 -0300

    Fix wrong OpenAI model name

--- backend/src/evaluators/unitxt.py
+++ backend/src/evaluators/unitxt.py
@@ -60,6 +60,7 @@ def get_inference_engine_params(
         inference_engine_params["extra_headers"] = {
             "RITS_API_KEY": credentials["api_key"]
         }
+    if provider == ModelProviderEnum.OPENAI:
         model_name = f"openai/{model_name}"
 
     inference_engine_params["model"] = model_name
