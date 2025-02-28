58a2d0e2aa7a34fdd7f2aa9c8ac02b783f1cff87
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2025-02-28 16:34:33 -0300

    Fix: backend host doesnt change

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,14 @@
+import os
+
 import uvicorn
 from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "src.main:app",
+        host=os.getenv("NEXT_PUBLIC_BACKEND_API_HOST", "127.0.0.1").split(":")[1][2:],
+        port=8000,
+        reload=True,
+    )
