a5a4f11ced10062be038ccd5b0134512374865e9
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2025-05-19 13:22:05 -0300

    Update script to get the db

--- cicd/get_db.sh
+++ cicd/get_db.sh
@@ -1,2 +1,2 @@
-BACKEND_POD=$(oc get pods -n eval-assist | grep 'backend' | awk '{print $1}' )
-oc cp eval-assist/${BACKEND_POD}:/app/prisma/db/dev.db ./dev.db
+BACKEND_POD=$(oc get pods -n eval-assist | grep 'backend' | head -n 1 | awk '{print $1}' )
+oc cp eval-assist/${BACKEND_POD}:/app/shared_volume/dev.db ./dev.db
