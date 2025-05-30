d41a0ed68e6532b0e92196afdc0b2c561576bea6
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2025-05-30 15:09:49 -0300

    Try downloading db until success

--- cicd/get_db.sh
+++ cicd/get_db.sh
@@ -1,2 +1,5 @@
-BACKEND_POD=$(oc get pods -n eval-assist-demo | grep 'backend' | head -n 1 | awk '{print $1}' )
-oc cp eval-assist-demo/${BACKEND_POD}:/app/shared_volume/dev.db ./dev.db
+BACKEND_POD=$(oc get pods -n eval-assist | grep 'backend' | head -n 1 | awk '{print $1}' )
+until oc cp eval-assist/${BACKEND_POD}:/app/shared_volume/dev.db ./dev.db; do
+  echo "Trying again"
+  sleep 1
+done
