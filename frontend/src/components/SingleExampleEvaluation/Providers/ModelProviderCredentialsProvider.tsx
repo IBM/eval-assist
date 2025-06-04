010799b756844a80992f67ad33effc4e0f858716
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2025-06-04 12:14:49 -0300

    Remove ollama's default value

--- frontend/src/components/SingleExampleEvaluation/Providers/ModelProviderCredentialsProvider.tsx
+++ frontend/src/components/SingleExampleEvaluation/Providers/ModelProviderCredentialsProvider.tsx
@@ -35,7 +35,7 @@ const defaultCrendentials = {
   [ModelProviderType.AWS]: { api_key: '' },
   [ModelProviderType.VERTEX_AI]: { api_key: '' },
   [ModelProviderType.REPLICATE]: { api_key: '' },
-  [ModelProviderType.OLLAMA]: { api_base: defaultOllamaURL },
+  [ModelProviderType.OLLAMA]: { api_base: '' },
 }
 interface ModelProviderCredentialsContextValue {
   modelProviderCredentials: ModelProviderCredentials
@@ -177,7 +177,7 @@ export const ModelProviderCredentialsProvider = ({ children }: { children: React
       Object.keys(getProviderCredentials(provider)).every((key) => {
         const credential = defaultCrendentials[provider]
         // @ts-ignore
-        return getProviderCredentials(provider)[key] !== '' // || defaultCrendentials[provider][key] !== ''
+        return getProviderCredentials(provider)[key] !== '' || defaultCrendentials[provider][key] !== ''
       }),
     [getProviderCredentials],
   )
