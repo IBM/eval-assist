edfc79ff3a48a10f4cdbc1bf9e4b2132ba045f8e
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2025-06-05 12:58:36 -0300

    Add jwt session config

--- frontend/src/pages/api/auth/[...nextauth].js
+++ frontend/src/pages/api/auth/[...nextauth].js
@@ -4,6 +4,10 @@ import Auth0 from 'next-auth/providers/auth0'
 // import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
   providers: [
     // GoogleProvider({
     //   clientId: process.env.GOOGLE_CLIENT_ID,
