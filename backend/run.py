d65e700994ab06761d81aa751d186fe47b6c0be8
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2025-06-14 21:29:10 -0300

    Prepare repo for pypi publishing
    Signed-off-by: Martín Santillán Cooper <martinsantillancooper@gmail.com>

--- backend/run.py
+++ backend/run.py
@@ -8,7 +8,9 @@ load_dotenv()
 if __name__ == "__main__":
     uvicorn.run(
         "evalassist.main:app",
-        host=os.getenv("NEXT_PUBLIC_BACKEND_API_HOST", "127.0.0.1").split(":")[1][2:],
+        host=os.getenv("NEXT_PUBLIC_BACKEND_API_HOST", "http://127.0.0.1:8000").split(
+            ":"
+        )[1][2:],
         port=8000,
-        reload=True,
+        reload=False,
     )
--- frontend/src/components/AppHeader/AppHeader.tsx
+++ frontend/src/components/AppHeader/AppHeader.tsx
@@ -2,7 +2,6 @@ import { PLATFORM_NAME } from 'src/constants'
 
 import { signOut } from 'next-auth/react'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
 
 import {
   Header,
@@ -12,7 +11,7 @@ import {
   HeaderName,
   HeaderNavigation,
 } from '@carbon/react'
-import { Document, LogoGithub, Logout, QqPlot } from '@carbon/react/icons'
+import { Document, LogoGithub, Logout } from '@carbon/react/icons'
 
 import { useAuthentication } from '@customHooks/useAuthentication'
 
@@ -21,12 +20,11 @@ import classes from './AppHeader.module.scss'
 export const AppHeader = () => {
   const title = `${PLATFORM_NAME}`
   const { authenticationEnabled, isAuthenticated } = useAuthentication()
-  const router = useRouter()
 
   return (
     <Header className={classes.root} aria-label={title}>
       <HeaderName prefix={''} style={{ cursor: 'pointer' }} href="/">
-        <Image width={16} height={16} src={`../images/logo.svg`} alt={'title'} style={{ marginRight: '0.25rem' }} />
+        <Image width={16} height={16} src={`/images/logo.svg`} alt={'title'} style={{ marginRight: '0.25rem' }} />
         {PLATFORM_NAME}
       </HeaderName>
 
