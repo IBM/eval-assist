ba779044f3b5ca4ec5f9009a77292de6cf758e13
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2025-07-25 13:32:27 -0300

    Typing improvements
    Signed-off-by: Martín Santillán Cooper <martinsantillancooper@gmail.com>

--- backend/src/evalassist/alembic/versions/004150514536_add_user_id_to_log_record.py
+++ backend/src/evalassist/alembic/versions/004150514536_add_user_id_to_log_record.py
@@ -6,16 +6,16 @@ Create Date: 2025-06-27 14:27:41.163987
 
 """
 
-from typing import Sequence, Union
+from typing import Sequence
 
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "004150514536"
-down_revision: Union[str, Sequence[str], None] = "6da8778318cb"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "004150514536"  # pragma: allowlist secret
+down_revision: str | Sequence[str] | None = "6da8778318cb"  # pragma: allowlist secret
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
