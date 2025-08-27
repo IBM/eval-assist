79012170e1fc3744da54733dc6f3726058421f10
Martín Santillán Cooper <marsancoo@gmail.com>
2025-08-27 12:06:18 -0300

    Exponse types in evalassist.judges module
    Signed-off-by: Martín Santillán Cooper <marsancoo@gmail.com>

--- backend/src/evalassist/judges/__init__.py
+++ backend/src/evalassist/judges/__init__.py
@@ -2,6 +2,15 @@ from .base import DirectJudge, Judge, PairwiseJudge
 from .dummy_judge import DummyDirectJudge, DummyPairwiseJudge
 from .simple_direct_judge import SimpleDirectJudge
 from .thesis_antithesis_direct_judge import ThesisAntithesisDirectJudge
+from .types import (
+    DirectInstance,
+    DirectInstanceResult,
+    DirectPositionalBias,
+    Instance,
+    PairwiseInstance,
+    PairwiseInstanceResult,
+    SingleSystemPairwiseResult,
+)
 from .unitxt_judges import UnitxtDirectJudge, UnitxtPairwiseJudge
 
 __all__: list[str] = [
@@ -14,4 +23,11 @@ __all__: list[str] = [
     "UnitxtPairwiseJudge",
     "DirectJudge",
     "PairwiseJudge",
+    "Instance",
+    "DirectInstance",
+    "PairwiseInstance",
+    "SingleSystemPairwiseResult",
+    "PairwiseInstanceResult",
+    "DirectPositionalBias",
+    "DirectInstanceResult",
 ]
