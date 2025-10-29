c1fec36824542be86d8d632da0a91405f5e7c8b7
Martín Santillán Cooper <marsancoo@gmail.com>
2025-10-29 14:37:46 -0300

    Add json object as the response format for litellm

--- backend/src/evalassist/judges/const.py
+++ backend/src/evalassist/judges/const.py
@@ -1,3 +1,13 @@
+from unitxt.inference import CrossProviderInferenceEngine, LiteLLMInferenceEngine
+
 DEFAULT_JUDGE_INFERENCE_PARAMS = {
     "temperature": 0,
+    # "stop": '}',
+    "provider_specific_args": {
+        k: {
+            "response_format": {"type": "json_object"},
+        }
+        for k, v in CrossProviderInferenceEngine._provider_to_base_class.items()
+        if v == LiteLLMInferenceEngine
+    },
 }
